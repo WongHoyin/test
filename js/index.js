91542bf24fd6174b80dd9a899f4f0ec835c18f7f
jonhaohe <jonhaohe>
2019-04-12 14:36:42 +0800

    首页第二次提交” git commit -m 首页第二次提交”

--- js/index.js
+++ js/index.js
@@ -1,3 +1,35 @@
 $(function () {
+  mui('.mui-scroll-wrapper').scroll({
+    deceleration: 0.0005, //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+    indicators: false //是否显示滚动条，默认为True
+  });
 
+
+  $.ajax({
+    type: 'get',
+    url: 'http://193.112.55.79:9090/api/getindexmenu',
+    // data: '',
+    dataType: 'json',
+    success: function (result) {
+      // console.log(result);
+      var html = template("listT", result)
+      // console.log(html);
+      $('.pyg_nav').html(html)
+    }
+  })
 })
+
+$.ajax({
+  type: 'get',
+  url: 'http://193.112.55.79:9090/api/getmoneyctrl',
+  // data: '',
+  dataType: 'json',
+  success: function (result) {
+    console.log(result);
+    var html = template("listCT", result)
+    $('.goodslist').html(html)
+
+  }
+})
+
+$()
